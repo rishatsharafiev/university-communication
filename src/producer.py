c7d5b5aaf835b24586e709f735fced478bb108c9
Rishat Sharafiev <rishatsharafiev@ya.ru>
2019-04-03 00:28:42 +0300

    Change producer handler

--- src/producer.py
+++ src/producer.py
@@ -8,12 +8,11 @@ from src import settings
 async def producer_handler(request):
     """Producer handler"""
     producer = AIOKafkaProducer(loop=settings.loop, bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS)
-
     await producer.start()
     try:
         message = await request.json()
 
-        if 'action' in message and message.get('action') == 'success' and 'url' in message:
+        if 'url' in message and 'source' in message:
             body = json.dumps(message).encode('utf-8')
             await producer.send_and_wait(settings.KAFKA_TOPIC_SUCCESS, body)
     except json.decoder.JSONDecodeError:
